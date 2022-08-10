12d96cb95588ba15a19b7f1f344a4db9030a7887
joao <joao.ribeiro@havan.com.br>
2022-08-10 19:50:40 -0300

    initial firebase

--- app/src/main/java/com/example/enquete/MainActivity.kt
+++ app/src/main/java/com/example/enquete/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.enquete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.enquete.data.model.Materia
+import com.example.enquete.presentation.adapter.MateriaAdapter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val db = Firebase.firestore
+    private val produtoAdapter by lazy { MateriaAdapter(mutableListOf()) }
+    private val lista: MutableList<Materia> = mutableListOf()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getMateria()
+    }
+
+    fun getMateria() {
+        try {
+            db.collection("materia-escolar")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val post = document.toObject(Materia::class.java)
+                        lista.add(post)
+                        initRecyclerView(lista)
+
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("teste", "Error getting documents.", exception)
+                }
+
+        } catch (ex: Exception) {
+            Log.w("teste", "Error getting documents.", ex)
+        }
+    }
+    fun initRecyclerView(prod: MutableList<Materia>) {
+        val recycler by lazy { findViewById<RecyclerView>(R.id.recyclerView) }
+        recycler.adapter = produtoAdapter
+        produtoAdapter.setDataSet(prod)
+        recycler.layoutManager = LinearLayoutManager(this)
+        produtoAdapter.notifyDataSetChanged()
+
     }
 }
