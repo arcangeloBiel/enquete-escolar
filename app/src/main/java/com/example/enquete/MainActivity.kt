ebfb2285906585dc727a63ecbdda79c8b7ba550e
joao <joao.ribeiro@havan.com.br>
2022-08-11 21:34:51 -0300

    update

--- app/src/main/java/com/example/enquete/MainActivity.kt
+++ app/src/main/java/com/example/enquete/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.enquete
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.enquete.data.model.Materia
+import com.example.enquete.data.utlis.ItemClickListener
 import com.example.enquete.presentation.adapter.MateriaAdapter
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -13,8 +16,9 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private val db = Firebase.firestore
-    private val produtoAdapter by lazy { MateriaAdapter(mutableListOf()) }
+    private val materiaAdapter by lazy { MateriaAdapter(mutableListOf()) }
     private val lista: MutableList<Materia> = mutableListOf()
+//
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         getMateria()
     }
+
 
     fun getMateria() {
         try {
@@ -45,10 +50,36 @@ class MainActivity : AppCompatActivity() {
     }
     fun initRecyclerView(prod: MutableList<Materia>) {
         val recycler by lazy { findViewById<RecyclerView>(R.id.recyclerView) }
-        recycler.adapter = produtoAdapter
-        produtoAdapter.setDataSet(prod)
+        recycler.adapter = materiaAdapter
+        materiaAdapter.setDataSet(prod)
         recycler.layoutManager = LinearLayoutManager(this)
-        produtoAdapter.notifyDataSetChanged()
+        eventoClickListener(materiaAdapter)
+        materiaAdapter.notifyDataSetChanged()
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    private fun eventoClickListener(pedidoItemAdapter: MateriaAdapter) {
+        pedidoItemAdapter.itemClickListener(object : ItemClickListener {
 
+            override fun onItemClick(view: View, position: Int) {
+                try {
+                    Log.d("position", " ${position}")
+                    when (view.id) {
+                       R.id.button -> {
+                           db.collection("materia-escolar").document(view.id.toString()).update(
+                            "voto", 2)
+                       }
+
+
+                    }
+
+                } catch (e: Exception) {
+                    Log.d("Abastecimento", " ${e.message}")
+
+                }
+               // pedidoItemAdapter.notifyDataSetChanged()
+              //  view.callOnClick()
+
+            }
+        })
     }
 }
